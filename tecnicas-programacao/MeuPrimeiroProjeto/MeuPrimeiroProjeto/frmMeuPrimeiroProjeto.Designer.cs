64dc4efd41d28e80edc8d4c5a635b36e78053b29
Leandro Ferreira <leandro@vipsolucoes.com>
2018-10-03 20:36:47 -0300

    Técnicas Programação - cria primeiro projeto no visual studio - 03/10/18

---
+++ tecnicas-programacao/MeuPrimeiroProjeto/MeuPrimeiroProjeto/frmMeuPrimeiroProjeto.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace MeuPrimeiroProjeto
+{
+    partial class frmMeuPrimeiroProjeto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrimeiroNome = new System.Windows.Forms.Label();
+            this.lblSobrenome = new System.Windows.Forms.Label();
+            this.txtPrimeiroNome = new System.Windows.Forms.TextBox();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPrimeiroNome
+            // 
+            this.lblPrimeiroNome.AutoSize = true;
+            this.lblPrimeiroNome.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrimeiroNome.Location = new System.Drawing.Point(12, 46);
+            this.lblPrimeiroNome.Name = "lblPrimeiroNome";
+            this.lblPrimeiroNome.Size = new System.Drawing.Size(118, 20);
+            this.lblPrimeiroNome.TabIndex = 5;
+            this.lblPrimeiroNome.Text = "Primeiro Nome:";
+            // 
+            // lblSobrenome
+            // 
+            this.lblSobrenome.AutoSize = true;
+            this.lblSobrenome.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSobrenome.Location = new System.Drawing.Point(38, 96);
+            this.lblSobrenome.Name = "lblSobrenome";
+            this.lblSobrenome.Size = new System.Drawing.Size(92, 20);
+            this.lblSobrenome.TabIndex = 6;
+            this.lblSobrenome.Text = "Sobrenome:";
+            // 
+            // txtPrimeiroNome
+            // 
+            this.txtPrimeiroNome.Location = new System.Drawing.Point(136, 41);
+            this.txtPrimeiroNome.Name = "txtPrimeiroNome";
+            this.txtPrimeiroNome.Size = new System.Drawing.Size(246, 25);
+            this.txtPrimeiroNome.TabIndex = 0;
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Location = new System.Drawing.Point(136, 91);
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.Size = new System.Drawing.Size(246, 25);
+            this.txtSobrenome.TabIndex = 1;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(136, 155);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(246, 25);
+            this.txtResultado.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Resultado:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnExecutar.FlatAppearance.BorderSize = 0;
+            this.btnExecutar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExecutar.Location = new System.Drawing.Point(168, 221);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(104, 48);
+            this.btnExecutar.TabIndex = 2;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = false;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Location = new System.Drawing.Point(278, 221);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(104, 48);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // frmMeuPrimeiroProjeto
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(398, 294);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.txtPrimeiroNome);
+            this.Controls.Add(this.lblSobrenome);
+            this.Controls.Add(this.lblPrimeiroNome);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "frmMeuPrimeiroProjeto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Meu Primeiro Projeto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrimeiroNome;
+        private System.Windows.Forms.Label lblSobrenome;
+        private System.Windows.Forms.TextBox txtPrimeiroNome;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ tecnicas-programacao/MeuPrimeiroProjeto/MeuPrimeiroProjeto/frmMeuPrimeiroProjeto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MeuPrimeiroProjeto
+{
+    public partial class frmMeuPrimeiroProjeto : Form
+    {
+        public frmMeuPrimeiroProjeto()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = $"{txtPrimeiroNome.Text.Trim()} {txtSobrenome.Text.Trim()}";
+
+            MessageBox.Show($"O resultado é {txtResultado.Text}", "Meu primeiro projeto",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
