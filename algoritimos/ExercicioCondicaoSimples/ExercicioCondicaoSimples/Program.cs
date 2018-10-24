b7bc1b80b510900b86b38952bd33677e506681bc
Leandro Ferreira <leandro@vipsolucoes.com>
2018-10-23 22:01:14 -0300

    Algoritimos - Exercícios estrutura de decisão simples - 23/10/18

---
+++ algoritimos/ExercicioCondicaoSimples/ExercicioCondicaoSimples/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExercicioCondicaoSimples
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int primeiroNumero;
+            int segundoNumero;
+
+            Console.WriteLine("Informe o primeiro número para somar:");
+            primeiroNumero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Informe o segundo número para somar:");
+            segundoNumero = int.Parse(Console.ReadLine());
+
+            var resultado = primeiroNumero + segundoNumero;
+
+            Console.Clear();
+
+            if (resultado > 10)
+                Console.WriteLine($"Resultado da soma {resultado}!");
+            else
+                Console.WriteLine("Soma não pode ser visualizada");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ algoritimos/ExercicioCondicaoSimples/ExercicioLerIdade/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExercicioLerIdade
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a idade desejada:");
+
+            var idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 18)
+                Console.WriteLine("É MAIOR de idade");
+            else
+                Console.WriteLine("É MENOR de idade.");
+
+            Console.ReadKey();
+        }
+    }
+}
