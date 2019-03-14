bd4640eef6215f12e7860ffe6db172c8a7567087
Leandro Ferreira <leandro@vipsolucoes.com>
2019-03-14 20:31:01 -0300

    "Técnicas de Programação - Atividade Navegador - 13/03/19"

---
+++ tecnicas-programacao/Navegador/Navegador/frmNavegador.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Navegador
+{
+    partial class frmNavegador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAvancar = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnIr = new System.Windows.Forms.Button();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(800, 450);
+            this.webBrowser.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSair);
+            this.panel1.Controls.Add(this.btnAvancar);
+            this.panel1.Controls.Add(this.btnVoltar);
+            this.panel1.Controls.Add(this.btnIr);
+            this.panel1.Controls.Add(this.txtEndereco);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 49);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnAvancar
+            // 
+            this.btnAvancar.Image = global::Navegador.Properties.Resources.icons8_circled_right_48;
+            this.btnAvancar.Location = new System.Drawing.Point(55, 13);
+            this.btnAvancar.Name = "btnAvancar";
+            this.btnAvancar.Size = new System.Drawing.Size(37, 26);
+            this.btnAvancar.TabIndex = 3;
+            this.btnAvancar.UseVisualStyleBackColor = true;
+            this.btnAvancar.Click += new System.EventHandler(this.btnAvancar_Click);
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Image = global::Navegador.Properties.Resources.icons8_go_back_48;
+            this.btnVoltar.Location = new System.Drawing.Point(12, 13);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(37, 26);
+            this.btnVoltar.TabIndex = 2;
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // btnIr
+            // 
+            this.btnIr.Location = new System.Drawing.Point(502, 11);
+            this.btnIr.Name = "btnIr";
+            this.btnIr.Size = new System.Drawing.Size(46, 26);
+            this.btnIr.TabIndex = 1;
+            this.btnIr.Text = "IR";
+            this.btnIr.UseVisualStyleBackColor = true;
+            this.btnIr.Click += new System.EventHandler(this.btnIr_Click);
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEndereco.Location = new System.Drawing.Point(102, 11);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(394, 26);
+            this.txtEndereco.TabIndex = 0;
+            this.txtEndereco.Text = "www.google.com";
+            this.txtEndereco.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtEndereco_KeyDown);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(699, 13);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(89, 26);
+            this.btnSair.TabIndex = 4;
+            this.btnSair.Text = "Fechar";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // frmNavegador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.webBrowser);
+            this.Name = "frmNavegador";
+            this.Text = "Navegador WEB";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmNavegador_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnIr;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnAvancar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ tecnicas-programacao/Navegador/Navegador/frmNavegador.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Navegador
+{
+    public partial class frmNavegador : Form
+    {
+        public frmNavegador()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIr_Click(object sender, EventArgs e)
+        {
+            var enderecoUrl = txtEndereco.Text.Trim();
+
+            if (string.IsNullOrEmpty(enderecoUrl))
+            {
+                MessageBox.Show("Informe o endereço e tente novamente.", "Navegador", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                return;
+            }
+
+            if (!enderecoUrl.Contains("http://"))
+                enderecoUrl = "http://" + enderecoUrl;
+
+            webBrowser.Url = new Uri(enderecoUrl);
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            if (webBrowser.CanGoBack)
+                webBrowser.GoBack();
+        }
+
+        private void btnAvancar_Click(object sender, EventArgs e)
+        {
+            if (webBrowser.CanGoForward)
+                webBrowser.GoForward();
+        }
+
+        private void txtEndereco_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnIr.PerformClick();
+                e.Handled = e.SuppressKeyPress = true;
+            }
+        }
+
+        private void frmNavegador_Load(object sender, EventArgs e)
+        {
+            btnIr.PerformClick();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
