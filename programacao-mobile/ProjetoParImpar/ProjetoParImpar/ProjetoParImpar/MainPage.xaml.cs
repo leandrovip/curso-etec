e97085530171eb7394f85163dd0434bebbe91b24
Leandro Ferreira <leandro@vipsolucoes.com>
2019-03-20 20:09:36 -0300

    Programação Mobile - Exercício xamarin par ou ímpar - 15/03/19

---
+++ programacao-mobile/ProjetoParImpar/ProjetoParImpar/ProjetoParImpar/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ProjetoParImpar
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnValidar_OnClicked(object sender, EventArgs e)
+        {
+            int.TryParse(txtValor.Text, out var valor);
+            var mensagem = valor % 2 == 0 ? "Número Par" : "Número Ímpar";
+
+            DisplayAlert("Resultado", mensagem, "Voltar");
+        }
+    }
+}
