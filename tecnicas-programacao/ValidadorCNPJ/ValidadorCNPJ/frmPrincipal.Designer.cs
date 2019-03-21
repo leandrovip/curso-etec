a66089c2ca5fb028d6ff93cdcfe141546ea73492
Leandro Ferreira <leandro@vipsolucoes.com>
2019-03-20 22:39:20 -0300

    Técnicas de Programação - Validador CNPJ - 20/03/19

---
+++ tecnicas-programacao/ValidadorCNPJ/ValidadorCNPJ/frmPrincipal.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ValidadorCNPJ
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(12, 69);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(445, 40);
+            this.txtValor.TabIndex = 0;
+            this.txtValor.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Arial", 20F);
+            this.lblTitulo.Location = new System.Drawing.Point(12, 34);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(324, 32);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "Informe o CNPJ desejado";
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValidar.Location = new System.Drawing.Point(18, 132);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(439, 61);
+            this.btnValidar.TabIndex = 2;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(469, 204);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.txtValor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPrincipal";
+            this.Text = "Validador de CNPJ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnValidar;
+    }
+}
+
---
+++ tecnicas-programacao/ValidadorCNPJ/ValidadorCNPJ/frmPrincipal.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ValidadorCNPJ
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            txtValor.Text = OnlyNumbers(txtValor.Text.Trim());
+
+            if (ValidarCnpj(txtValor.Text))
+                MessageBox.Show("CNPJ válido! :)", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("CNPJ inválido! :(", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            txtValor.SelectAll();
+            txtValor.Focus();
+        }
+
+        private bool ValidarCnpj(string valor)
+        {
+            #region Validações
+
+            if (string.IsNullOrEmpty(valor))
+                return false;
+
+            valor = OnlyNumbers(valor);
+            if (valor.Length != 14)
+                return false;
+
+            #endregion
+
+            var listaDigito1 = new[] {5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+            var listaDigito2 = new[] {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+
+            var digito1 = CalculateDigits(listaDigito1, valor);
+            var digito2 = CalculateDigits(listaDigito2, valor);
+
+            return valor.Substring(12, 2) == $"{digito1}{digito2}";
+        }
+
+        private int CalculateDigits(IEnumerable<int> lista, string valor)
+        {
+            var soma = 0;
+            var i = 0;
+
+            foreach (var numero in lista)
+            {
+                soma += int.Parse(valor[i].ToString()) * numero;
+                i++;
+            }
+
+            var resultado = soma % 11;
+            if (resultado < 2)
+                return 0;
+
+            return 11 - resultado;
+        }
+
+        private string OnlyNumbers(string value)
+        {
+            return string.IsNullOrEmpty(value) ? "" : new string(value.Where(char.IsDigit).ToArray());
+        }
+    }
+}
