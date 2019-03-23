f607a7db0329831b70f0de5aa446ad0798a487c8
Leandro Ferreira <leandro@vipsolucoes.com>
2019-03-22 21:33:02 -0300

    Técnicas de Programação - Exercício Par/Impar com validação - 22/03/19

---
+++ tecnicas-programacao/ExercicioParImpar/ExercicioParImpar/frmPrincipal.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ExercicioParImpar
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(18, 82);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(362, 53);
+            this.txtValor.TabIndex = 0;
+            this.txtValor.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(12, 24);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(368, 31);
+            this.lblTitulo.TabIndex = 3;
+            this.lblTitulo.Text = "Informe o número desejado";
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValidar.Location = new System.Drawing.Point(18, 164);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(176, 54);
+            this.btnValidar.TabIndex = 1;
+            this.btnValidar.Text = "VALIDAR";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(200, 164);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(180, 54);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(394, 238);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.txtValor);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPrincipal";
+            this.Text = "Exercício Par/Ímpar com Tratamentos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ tecnicas-programacao/ExercicioParImpar/ExercicioParImpar/frmPrincipal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ExercicioParImpar
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            if (!SeValido()) return;
+
+            decimal.TryParse(txtValor.Text, out var valor);
+
+            MessageBox.Show(valor % 2 == 0 ? "Número PAR" : "Número ÍMPAR", "Resultado", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+            txtValor.SelectAll();
+            txtValor.Focus();
+        }
+
+        private bool SeValido()
+        {
+            if (string.IsNullOrEmpty(txtValor.Text))
+            {
+                MessageBox.Show("Informe o número desejado e tente novamente;", "Par/Ímpar", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                txtValor.SelectAll();
+                txtValor.Focus();
+                return false;
+            }
+
+            decimal.TryParse(txtValor.Text, out var resultado);
+            if (resultado == 0)
+            {
+                MessageBox.Show("Informe o número válido e tente novamente;", "Par/Ímpar", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                txtValor.SelectAll();
+                txtValor.Focus();
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
