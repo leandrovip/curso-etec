4a9e9f5b14e7991539ca9ba5cde1c18e24672517
Leandro Ferreira <leandro@vipsolucoes.com>
2019-05-16 19:52:52 -0300

    Técnicas de Programação - Exercício Métodos - 15/05/2019

---
+++ tecnicas-programacao/ExercicioMetodos/ExercicioMetodos/frmPrincipal.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ExercicioMetodos
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMensagem = new System.Windows.Forms.Button();
+            this.dtpHorario = new System.Windows.Forms.DateTimePicker();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMensagem
+            // 
+            this.btnMensagem.Location = new System.Drawing.Point(64, 64);
+            this.btnMensagem.Name = "btnMensagem";
+            this.btnMensagem.Size = new System.Drawing.Size(185, 108);
+            this.btnMensagem.TabIndex = 0;
+            this.btnMensagem.Text = "Clique aqui!";
+            this.btnMensagem.UseVisualStyleBackColor = true;
+            this.btnMensagem.Click += new System.EventHandler(this.btnMensagem_Click);
+            // 
+            // dtpHorario
+            // 
+            this.dtpHorario.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpHorario.Location = new System.Drawing.Point(215, 17);
+            this.dtpHorario.MinDate = new System.DateTime(1969, 12, 25, 0, 0, 0, 0);
+            this.dtpHorario.Name = "dtpHorario";
+            this.dtpHorario.Size = new System.Drawing.Size(89, 20);
+            this.dtpHorario.TabIndex = 1;
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensagem.Location = new System.Drawing.Point(12, 20);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(197, 17);
+            this.lblMensagem.TabIndex = 2;
+            this.lblMensagem.Text = "Selecione o horário desejado:";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 196);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.dtpHorario);
+            this.Controls.Add(this.btnMensagem);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPrincipal";
+            this.Text = "Exercício Métodos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMensagem;
+        private System.Windows.Forms.DateTimePicker dtpHorario;
+        private System.Windows.Forms.Label lblMensagem;
+    }
+}
---
+++ tecnicas-programacao/ExercicioMetodos/ExercicioMetodos/frmPrincipal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ExercicioMetodos
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMensagem_Click(object sender, EventArgs e)
+        {
+            var hora = dtpHorario.Value.Hour;
+            VerificarHorario(hora);
+        }
+
+        private void VerificarHorario(int hora)
+        {
+            if (hora >= 18)
+                Mensagem("Boa Noite :)");
+            else if (hora >= 12)
+                Mensagem("Boa Tarde :P");
+            else
+                Mensagem("Bom dia Maluco!");
+        }
+
+        private void Mensagem(string mensagem)
+        {
+            MessageBox.Show(mensagem, "Olá! :)", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
