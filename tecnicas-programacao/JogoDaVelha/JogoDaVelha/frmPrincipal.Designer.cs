19e3dc96f9111ec2e302548034c6474ce5807296
Leandro Ferreira <leandro@vipsolucoes.com>
2019-05-22 22:03:15 -0300

    Técnicas de Programação - Jogo da Velha - 22/05/19

---
+++ tecnicas-programacao/JogoDaVelha/JogoDaVelha/frmPrincipal.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace JogoDaVelha
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.lblProximoJogador = new System.Windows.Forms.Label();
+            this.lblJogador = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.btn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn1.Enabled = false;
+            this.btn1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(140)))), ((int)(((byte)(141)))));
+            this.btn1.FlatAppearance.BorderSize = 2;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Font = new System.Drawing.Font("Arial Black", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.btn1.Location = new System.Drawing.Point(12, 83);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(125, 109);
+            this.btn1.TabIndex = 0;
+            this.btn1.Tag = "True";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.btn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn2.Enabled = false;
+            this.btn2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(140)))), ((int)(((byte)(141)))));
+            this.btn2.FlatAppearance.BorderSize = 2;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("Arial Black", 39.75F, System.Drawing.FontStyle.Bold);
+            this.btn2.Location = new System.Drawing.Point(182, 83);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(125, 109);
+            this.btn2.TabIndex = 0;
+            this.btn2.Tag = "True";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.btn3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn3.Enabled = false;
+            this.btn3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(140)))), ((int)(((byte)(141)))));
+            this.btn3.FlatAppearance.BorderSize = 2;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Font = new System.Drawing.Font("Arial Black", 39.75F, System.Drawing.FontStyle.Bold);
+            this.btn3.Location = new System.Drawing.Point(348, 83);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(125, 109);
+            this.btn3.TabIndex = 0;
+            this.btn3.Tag = "True";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.btn4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn4.Enabled = false;
+            this.btn4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(140)))), ((int)(((byte)(141)))));
+            this.btn4.FlatAppearance.BorderSize = 2;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.Font = new System.Drawing.Font("Arial Black", 39.75F, System.Drawing.FontStyle.Bold);
+            this.btn4.Location = new System.Drawing.Point(12, 232);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(125, 109);
+            this.btn4.TabIndex = 0;
+            this.btn4.Tag = "True";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.btn5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn5.Enabled = false;
+            this.btn5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(140)))), ((int)(((byte)(141)))));
+            this.btn5.FlatAppearance.BorderSize = 2;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5.Font = new System.Drawing.Font("Arial Black", 39.75F, System.Drawing.FontStyle.Bold);
+            this.btn5.Location = new System.Drawing.Point(182, 232);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(125, 109);
+            this.btn5.TabIndex = 0;
+            this.btn5.Tag = "True";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.btn6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn6.Enabled = false;
+            this.btn6.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(140)))), ((int)(((byte)(141)))));
+            this.btn6.FlatAppearance.BorderSize = 2;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn6.Font = new System.Drawing.Font("Arial Black", 39.75F, System.Drawing.FontStyle.Bold);
+            this.btn6.Location = new System.Drawing.Point(348, 232);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(125, 109);
+            this.btn6.TabIndex = 0;
+            this.btn6.Tag = "True";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.btn7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn7.Enabled = false;
+            this.btn7.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(140)))), ((int)(((byte)(141)))));
+            this.btn7.FlatAppearance.BorderSize = 2;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn7.Font = new System.Drawing.Font("Arial Black", 39.75F, System.Drawing.FontStyle.Bold);
+            this.btn7.Location = new System.Drawing.Point(12, 381);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(125, 109);
+            this.btn7.TabIndex = 0;
+            this.btn7.Tag = "True";
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.btn8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn8.Enabled = false;
+            this.btn8.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(140)))), ((int)(((byte)(141)))));
+            this.btn8.FlatAppearance.BorderSize = 2;
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn8.Font = new System.Drawing.Font("Arial Black", 39.75F, System.Drawing.FontStyle.Bold);
+            this.btn8.Location = new System.Drawing.Point(182, 381);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(125, 109);
+            this.btn8.TabIndex = 0;
+            this.btn8.Tag = "True";
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.btn9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn9.Enabled = false;
+            this.btn9.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(140)))), ((int)(((byte)(141)))));
+            this.btn9.FlatAppearance.BorderSize = 2;
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn9.Font = new System.Drawing.Font("Arial Black", 39.75F, System.Drawing.FontStyle.Bold);
+            this.btn9.Location = new System.Drawing.Point(348, 381);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(125, 109);
+            this.btn9.TabIndex = 0;
+            this.btn9.Tag = "True";
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.btnIniciar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciar.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.btnIniciar.Location = new System.Drawing.Point(12, 12);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(125, 39);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // lblProximoJogador
+            // 
+            this.lblProximoJogador.AutoSize = true;
+            this.lblProximoJogador.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.lblProximoJogador.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.lblProximoJogador.Location = new System.Drawing.Point(208, 19);
+            this.lblProximoJogador.Name = "lblProximoJogador";
+            this.lblProximoJogador.Size = new System.Drawing.Size(221, 32);
+            this.lblProximoJogador.TabIndex = 2;
+            this.lblProximoJogador.Text = "Próximo Jogador:";
+            // 
+            // lblJogador
+            // 
+            this.lblJogador.AutoSize = true;
+            this.lblJogador.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.lblJogador.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(43)))));
+            this.lblJogador.Location = new System.Drawing.Point(451, 19);
+            this.lblJogador.Name = "lblJogador";
+            this.lblJogador.Size = new System.Drawing.Size(22, 32);
+            this.lblJogador.TabIndex = 2;
+            this.lblJogador.Text = ".";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(195)))), ((int)(((byte)(199)))));
+            this.ClientSize = new System.Drawing.Size(485, 502);
+            this.Controls.Add(this.lblJogador);
+            this.Controls.Add(this.lblProximoJogador);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Velha - 1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label lblProximoJogador;
+        private System.Windows.Forms.Label lblJogador;
+    }
+}
---
+++ tecnicas-programacao/JogoDaVelha/JogoDaVelha/frmPrincipal.cs
@@ -0,0 +1,176 @@
+﻿// Leandro Ferreira
+// www.github.com/leandrovip
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace JogoDaVelha
+{
+    public partial class frmPrincipal : Form
+    {
+        #region Propriedades
+
+        private string _ultimoJogador = "";
+        private int _jogadasRestantes = 9;
+
+        #endregion
+
+        #region Construtor
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Eventos
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja iniciar um novo Jogo ?", "Jogo da Velha", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question) == DialogResult.No)
+            {
+                BloquearBotoes();
+                return;
+            }
+
+            lblJogador.Text = "X";
+            _jogadasRestantes = 9;
+            _ultimoJogador = "";
+            LimparBotoes();
+        }
+
+        private void btnJogo_Click(object sender, EventArgs e)
+        {
+            var botao = (Button) sender;
+            botao.Text = _ultimoJogador == "X" ? "O" : "X";
+            botao.Enabled = false;
+            _ultimoJogador = botao.Text;
+            lblJogador.Text = _ultimoJogador == "X" ? "O" : "X";
+            _jogadasRestantes -= 1;
+
+            if (TemVencedor())
+            {
+                MessageBox.Show($"O Jogador {_ultimoJogador} venceu! Parabéns :)", "Aê sim", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+
+                btnIniciar.PerformClick();
+                return;
+            }
+
+            if (_jogadasRestantes == 0)
+            {
+                MessageBox.Show("Ninguém venceu! Amebas!", "Que feio", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                btnIniciar.PerformClick();
+            }
+        }
+
+        #endregion
+
+        #region Métodos
+
+        private void LimparBotoes()
+        {
+            foreach (var button in Controls.OfType<Button>())
+                if (button.Tag != null)
+                {
+                    button.Text = "";
+                    button.BackColor = Color.FromArgb(241, 196, 15);
+                    button.Enabled = true;
+                }
+        }
+
+        private void BloquearBotoes()
+        {
+            foreach (var button in Controls.OfType<Button>())
+                if (button.Tag != null)
+                    button.Enabled = false;
+        }
+
+        private void MarcarVencedor(IReadOnlyList<string> indices)
+        {
+            foreach (var button in Controls.OfType<Button>())
+                if (button.Tag != null)
+                    for (var i = 0; i < 3; i++)
+                        if (button.Name == $"btn{indices[i]}")
+                            button.BackColor = Color.FromArgb(46, 204, 113);
+        }
+
+        private bool TemVencedor()
+        {
+            if (VerificaDiagonal() || VerificaHorizontal() || VerificaVertical())
+                return true;
+
+            return false;
+        }
+
+        private bool VerificaHorizontal()
+        {
+            if (btn1.Text == _ultimoJogador && btn2.Text == _ultimoJogador && btn3.Text == _ultimoJogador)
+            {
+                MarcarVencedor(new[] {"1", "2", "3"});
+                return true;
+            }
+
+            if (btn4.Text == _ultimoJogador && btn5.Text == _ultimoJogador && btn6.Text == _ultimoJogador)
+            {
+                MarcarVencedor(new[] {"4", "5", "6"});
+                return true;
+            }
+
+            if (btn7.Text == _ultimoJogador && btn8.Text == _ultimoJogador && btn9.Text == _ultimoJogador)
+            {
+                MarcarVencedor(new[] {"7", "8", "9"});
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool VerificaVertical()
+        {
+            if (btn1.Text == _ultimoJogador && btn4.Text == _ultimoJogador && btn7.Text == _ultimoJogador)
+            {
+                MarcarVencedor(new[] {"1", "4", "7"});
+                return true;
+            }
+
+            if (btn2.Text == _ultimoJogador && btn5.Text == _ultimoJogador && btn8.Text == _ultimoJogador)
+            {
+                MarcarVencedor(new[] {"2", "5", "8"});
+                return true;
+            }
+
+            if (btn3.Text == _ultimoJogador && btn6.Text == _ultimoJogador && btn9.Text == _ultimoJogador)
+            {
+                MarcarVencedor(new[] {"3", "6", "9"});
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool VerificaDiagonal()
+        {
+            if (btn1.Text == _ultimoJogador && btn5.Text == _ultimoJogador && btn9.Text == _ultimoJogador)
+            {
+                MarcarVencedor(new[] {"1", "5", "9"});
+                return true;
+            }
+
+            if (btn3.Text == _ultimoJogador && btn5.Text == _ultimoJogador && btn7.Text == _ultimoJogador)
+            {
+                MarcarVencedor(new[] {"3", "5", "7"});
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+    }
+}
